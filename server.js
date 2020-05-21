c86e9f509f3246c35ba79594f141a092c26e4dff
kalyan <kalyanveenam@gmail.com>
2020-05-21 17:49:26 +0530

    MAINT : Initial commit

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require('path');
-app.use(express.static(__dirname + '/dist'))
-app.listen(process.env.PORT || 8000);
-app.get('/*', function (req, res) {
-    res.sendfile(path.join(__dirname + '/dist/index.html'))
-})
-console.log('console listening')
